8c1b0e0d3284471f9dd1712fe3711fb872363ea7
Luiz <zluizfs.outlook.com>
2021-11-01 19:30:35 -0300

    Autenticação de usuários

--- Controllers/AutenticacaoController.cs
+++ Controllers/AutenticacaoController.cs
@@ -74,6 +74,26 @@ namespace eCommerce.Controllers
       return View(model);
     }
 
+    [HttpPost]
+    [AllowAnonymous]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Acessar(AcessarViewModel model, string returnUrl = null)
+    {
+      ViewData["ReturnUrl"] = returnUrl;
+      if (ModelState.IsValid)
+      {
+        var result = await _signInManager.PasswordSignInAsync(model.Email, model.Senha, model.LembrarDeMim, lockoutOnFailure: false);
+        if (result.Succeeded)
+        {
+          _logger.LogInformation("Usuário Autenticado.");
+          return RedirectToLocal(returnUrl);
+        }
+      }
+      ModelState.AddModelError(string.Empty, "Falha na tentativa de login.");
+      return View(model);
+    }
+
+
     private void AddErrors(IdentityResult result)
     {
       foreach (var error in result.Errors)
@@ -94,7 +114,14 @@ namespace eCommerce.Controllers
       }
     }
 
+    public async Task<IActionResult> Sair()
+    {
+      await _signInManager.SignOutAsync();
+      _logger.LogInformation("Usuário realizou logout.");
+      return RedirectToAction(nameof(HomeController.Index), "Home");
+    }
 
 
   }
+
 }
